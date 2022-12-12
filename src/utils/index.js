c7d032bb4ee568f3f3ab8a11f455849e39bbfb72
yamid-granda <yamid.granda@gmail.com>
2022-12-11 21:42:32 -0500

    feat: max 2 decimals in numbers

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,7 @@
 export function formatNumber(number) {
-  const numberFormatConfig = { useGrouping: false }
+  const numberFormatConfig = {
+    useGrouping: false,
+    maximumFractionDigits: 2,
+  }
   return new Intl.NumberFormat('en-US', numberFormatConfig).format(number)
 }
